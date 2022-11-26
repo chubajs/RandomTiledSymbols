efbef6f0728659e1895d6721deb1b3930b053b3b
chubajs <chubajs@gmail.com>
2022-11-26 08:15:12 +0400

    Initial Commit

---
+++ RandomTiledSymbols/RandomTiledSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTiledSymbolsApp.swift
+//  RandomTiledSymbols
+//
+//  Created by Sergey Bulaev on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomTiledSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
